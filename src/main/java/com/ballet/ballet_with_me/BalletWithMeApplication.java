729f6d2c192f3f9e86a0dabf22546078dc4a9c6f
jjoo <zhuyaan93@gmail.com>
2024-11-25 16:40:04 +0900

    first commit

---
+++ src/main/java/com/ballet/ballet_with_me/BalletWithMeApplication.java
@@ -0,0 +1,13 @@
+package com.ballet.ballet_with_me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalletWithMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BalletWithMeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ballet/ballet_with_me/BalletWithMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ballet.ballet_with_me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalletWithMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
